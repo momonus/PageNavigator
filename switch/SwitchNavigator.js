924f91fb5102efb29c38706f2cf67f042367732d
momonus <53513564+momonus@users.noreply.github.com>
2019-08-28 16:46:49 +0700

    Update SwitchNavigator.js

--- switch/SwitchNavigator.js
+++ switch/SwitchNavigator.js
@@ -1,4 +1,4 @@
-import { createSwitchNavigator,createAppContainer,reateStackNavigator } from 'react-navigation';
+import { createSwitchNavigator,createAppContainer,createStackNavigator } from 'react-navigation';
 import Page1 from './Page1'
 import Page2 from './Page2'
 import Page3 from './Page3'
